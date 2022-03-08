df2787b8837c74960015fcb67790285ef3eba5be
Sensodyne Toothpaste <webdevelopercs976@gmail.com>
2022-03-08 14:25:27 -0500

    fixed editText by getting rid of the descentFocusability="blockDescendants" feature in activitymain.xml

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -4,42 +4,55 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // set content of this activity from activity_main.xml layout
-        setContentView(R.layout.activity_main)
 
-        //click button --> do something
-        //it is the done button which is the "view" passed as the argument
-        //button is an extension of TextView
-        findViewById<Button>(R.id.done_button).setOnClickListener {addNickename(it)
+        //replace setContent view with data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //we can now access done button using binding
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
         }
     }
 
     //The current issue with calling editText is that it creates a lot of overhead for our mainactivity class
     //We will fix this by using something called data binding
-    private fun addNickename(view: View) {
-        //view is our button
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+    private fun addNickname(view: View) {
 
-        nicknameTextView.text = editText.text
-        //hide edittext and button after input
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        //make typed nickname visible instead
-        nicknameTextView.visibility = View.VISIBLE
+        //make code easier to read using binding.apply
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            //invalidates all binding expressions so they get recreated with correct data
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
 
         //hide keyboard after done typing
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
 
+/* Why use data binding?
+-generates binding objects at compile times for all views in the layout
+-accessing views through binding object is more efficient than findViewById
+-for complex hierarchies, data binding can be significant performance gain
+-findViewbyId traverses the view hierarchy at runntime to find a find everytime it's called
+ */
